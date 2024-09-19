01b5ec5f4cb3f3145145237143e7217319aa85dc
arnau.bori <arnau.bori@auleseebe.upc.edu>
2024-09-19 13:22:07 +0200

    P1

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,32 @@
 #Codigo para problema 1
 
-plot(1:10)
+mis_dades <- mtcars
+qsec_intervalos <- cut(mis_dades$qsec, 4)
+ni <- table(qsec_intervalos)
+ni
+sum(ni)
+hist(mis_dades$qsec,5)
+fi <- ni/sum(ni)
+pie(fi)
+cumsum(ni)
+Fi <- ni/sum(ni)
+cbind(ni, fi, Fi)
+hist(mis_dades$qsec)
+mean(mis_dades$qsec)
+
+sort(mis_dades$drat)
+median(mis_dades$drat)
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
+
+quantile(mis_dades$drat, 0,18)
+
+quantile(mis_dades$mpg, 0.75)-quantile(mis_dades$mpg, 0.25)
+IQR(mis_dades$mpg)
+boxplot(mis_dades$mpg)
+
+sort(mis_dades$cyl)
+IQR(mis_dades$cyl)
+boxplot(mis_dades$cyl)
+sd(mis_dades$cyl)
+var(mis_dades$cyl)
